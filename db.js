699f84fcae85ed07ad9f7df974e73acf52fca7c3
satuhin95 <saifcm95@gmail.com>
2022-08-31 15:14:53 +0600

    update tags data

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const todos = require("./data/todos");
 const videos = require("./data/videos");
+const tags = require("./data/tags");
 const transactions = require("./data/transactions");
 
 module.exports = {
