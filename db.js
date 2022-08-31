8fc26c2a88a79de0f235101de718ac76381e3493
satuhin95 <saifcm95@gmail.com>
2022-08-31 14:53:50 +0600

    fake json server

---
+++ db.js
@@ -0,0 +1,10 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  transactions,
+}
+
